80778a6bc362b24ab214cd9c726c843e063de62b
matdehaven <matthew_dehaven@brown.edu>
2023-06-06 14:47:55 -0400

    Initial setup

---
+++ R/id_td.R
@@ -0,0 +1,18 @@
+#' Identify...
+#'
+#' @param var vars VAR ovject
+#'
+#' @return structural var
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' svar <- id_td(var)
+#' }
+id_td <- function(var) {
+
+  ## To do
+  svar <- var
+
+  return(svar)
+}
